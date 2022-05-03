dc2193d1eaab6ab65b40d77c8ea8394fb8339f02
David Hunter <dhunter@accretivetg.com>
2022-05-03 21:51:43 +0000

    updated

--- capthook/interface_updater.py
+++ capthook/interface_updater.py
@@ -4,6 +4,20 @@ import os
 
 payload = os.environ["PAYLOAD"]
 
+PLATFORM_MAP = {
+    "leaf1": "arista_eos",
+    "leaf2": "arista_eos",
+    "leaf3": "cisco_nxos",
+    "spine1": "arista_eos",
+}
+
+NAT_MAP = {
+    "leaf1": "2221",
+    "leaf2": "2222",
+    "leaf3": "2223",
+    "spine1": "2211",
+}
+
 def main():
     #print("hi")
     #with open("webhook_payload.json", "r") as f:
@@ -25,19 +39,21 @@ def main():
         "platform": "arista_eos",
         "transport": "paramiko"
     }
+    with Scrapli(host="student-davidh.us-west1-a", port=NAT_MAP[device_changed], auth_username="admin", auth_password="admin" auth_strict_key=False, platform=PLATFORM_MAP[device_changed], transport="paramiko") as conn:
+        configs = ['interface ' + intf_changed, 'description ' + new_descr]
+        response = conn.send_configs(configs)
 
-    conn = Scrapli(**device)
-    conn.open()
-    print(conn.get_prompt())
-    configs = ['interface ' + intf_changed, 'description ' + new_descr]
-    response = conn.send_configs(configs)
-    print(response, type(response), dir(response))
-    print(response.result)
-    for response in response:
-        print(response, type(response), dir(response))
-        print(response.result)
 
-    conn.close()
+#    conn = Scrapli(**device)
+#    conn.open()
+#    print(conn.get_prompt())
+#    print(response, type(response), dir(response))
+#    print(response.result)
+#    for response in response:
+#        print(response, type(response), dir(response))
+#        print(response.result)
+#
+#    conn.close()
 
 
 if __name__ == "__main__":
