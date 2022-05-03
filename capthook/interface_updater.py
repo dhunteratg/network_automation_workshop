69acd1bbf2e2d6706a5b355108fcd041f21ccfb1
David Hunter <dhunter@accretivetg.com>
2022-05-03 21:14:14 +0000

    added interface_updater.py

---
+++ capthook/interface_updater.py
@@ -0,0 +1,39 @@
+import json
+from scrapli import Scrapli
+
+def main():
+    print("hi")
+    with open("webhook_payload.json", "r") as f:
+        netbox_payload = json.load(f)
+
+    device_changed = netbox_payload["data"]["device"]["name"]
+    intf_changed = netbox_payload["data"]["name"]
+    new_descr = netbox_payload["data"]["description"]
+
+    print(f"device {device_changed} interface {intf_changed} updated description to: {new_descr}")
+
+    device = {
+        "host": device_changed,
+        "auth_username": "admin",
+        "auth_password": "admin",
+        "auth_strict_key": False,
+        "platform": "arista_eos"
+        #"transport": "paramiko"
+    }
+
+    conn = Scrapli(**device)
+    conn.open()
+    print(conn.get_prompt())
+    configs = ['interface ' + intf_changed, 'description ' + new_descr]
+    response = conn.send_configs(configs)
+    print(response, type(response), dir(response))
+    print(response.result)
+    for response in response:
+        print(response, type(response), dir(response))
+        print(response.result)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
